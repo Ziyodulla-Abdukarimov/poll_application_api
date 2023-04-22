46daed585e21211f81e463ca9567023315e4511a
Ziyodulla-Abdukarimov <ZiyodullaDeveloper@gmail.com>
2023-04-22 22:04:38 +0500

    MultiChoice added

--- model/admin.py
+++ model/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Question, Choice, OneChoice, MultiChoice
+
 
 # Register your models here.
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(OneChoice)
+admin.site.register(MultiChoice)
