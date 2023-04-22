c07eecfb2244f68b10d09e2fc8ae1c09669e5f7d
Ziyodulla-Abdukarimov <ZiyodullaDeveloper@gmail.com>
2023-04-22 15:06:28 +0500

    APIs have been created

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import *
+from rest_framework.authtoken.models import Token
 
 
 class QuestionSerializer(serializers.ModelSerializer):
@@ -12,3 +13,20 @@ class ChoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Choice
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        Token.objects.create(user=user)
+        return user
--- api/views.py
+++ api/views.py
@@ -1,9 +1,33 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
+from django.contrib.auth import authenticate
 from .models import *
 from .serializers import *
+
+
+class UserCreate(CreateAPIView):
+    authentication_classes = ()
+    permission_classes = ()
+
+    serializer_class = UserSerializer
+
+
+class LoginView(APIView):
+    permission_classes = ()
+
+    def post(self, request, ):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+
+        if user:
+            return Response({"token": user.auth_token.key})
+        else:
+            return Response({"error": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 # Create your views here.
