1c651e1e8c79174f9f5bfcc3068cbb0e63c9904c
Ziyodulla-Abdukarimov <ZiyodullaDeveloper@gmail.com>
2023-04-22 15:12:48 +0500

    Swagger added

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
     path('api-auth/', include('rest_framework.urls')),
-
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
